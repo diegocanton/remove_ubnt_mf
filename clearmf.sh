a8ebb95a1ba678899a124b114c96407ddd7ad3ed
diegocanton <diegocantonbgi@gmail.com>
2016-05-13 23:11:29 -0300

    Update clearmf.sh

--- clearmf.sh
+++ clearmf.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+# use clearmf.sh PASS USER NETWORK INITIAL_IP
 
-pass=YOURPASS
-user=YOURUSER
-#Initial IP
-ip=2
-
-#Network
-network="10.100.7."
+if ["$1"]; then
+	pass=YOURPASS
+fi
+if ["$2"]; then
+	user=YOURUSER
+fi
+if ["$3"]; then
+	#Network
+	network="10.100.7."
+fi
+if ["$4"]; then
+	#Initial IP
+	ip=1
+fi
 
 while [ $ip -lt 255 ]; do
   #SSH connection and execution of desinfect.sh in GIT
