dbd920aaf510d337c033efa9c8d261a26f458f18
diegocanton <diegocantonbgi@gmail.com>
2016-05-13 23:36:14 -0300

    Update clearmf.sh

--- clearmf.sh
+++ clearmf.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 # use clearmf.sh PASS USER NETWORK INITIAL_IP
 
-if [$1]; then
-	pass=YOURPASS
+pass=$1
+user=$2
+network=$3
+ip=$4
+
+if [ -z $1]; then
+        pass=YOURPASS
 fi
-if [$2]; then
-	user=YOURUSER
+if [ -z $2]; then
+        user=YOURUSER
 fi
-if [$3]; then
-	#Network
-	network="10.100.7."
+if [ -z $3]; then
+        #Network
+        network="10.100.7."
 fi
-if [$4]; then
-	#Initial IP
-	ip=1
+if [ -z $4]; then
+        #Initial IP
+        ip=1
 fi
 
 while [ $ip -lt 255 ]; do
   #SSH connection and execution of desinfect.sh in GIT
-	sshpass -p $pass ssh $user@$network$ip "wget -qO- https://raw.githubusercontent.com/diegocanton/remove_ubnt_mf/master/desinfect.sh | sh"
+        sshpass -p $pass ssh $user@$network$ip "wget -qO- https://raw.githubusercontent.com/diegocanton/remove_ubnt_mf/master/desinfect.sh | sh"
 done
