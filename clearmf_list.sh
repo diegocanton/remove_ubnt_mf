a60da747cfb3c75879350c5fffcd3e04470957ec
diegocanton <diegocantonbgi@gmail.com>
2016-05-15 09:19:22 -0300

    Update clearmf_list.sh

--- clearmf_list.sh
+++ clearmf_list.sh
@@ -6,6 +6,11 @@
 # tcpdump -v -n port 53 -i eth1 | grep -i nxdomain | cut -f5-8 -d "." | cut -f3 -d " " > /opt/remove_ubnt_mf/iplist2.txt
 # cat /opt/remove_ubnt_mf/iplist2.txt | sort -n | uniq > /opt/remove_ubnt_mf/iplist.txt
 
+#In iplist.txt use:
+#10.0.0.1
+#10.0.0.4
+#...
+
 _input=/opt/remove_ubnt_mf/iplist.txt
 pass=$1
 user=$2
