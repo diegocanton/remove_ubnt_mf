87950ca8348105d389c7742697b212ea7dfffe6d
diegocanton <diegocantonbgi@gmail.com>
2016-05-15 09:21:49 -0300

    Create clearmf_net.sh

---
+++ clearmf_net.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Example of user clearmf.sh automation
+
+# All examples use default user and pass of vírus, and work in mf infecion
+
+/opt/remove_ubnt_mf/clearmf.sh fucker mother 10.0.0 1
+/opt/remove_ubnt_mf/clearmf.sh fucker mother 10.0.1 1
+/opt/remove_ubnt_mf/clearmf.sh fucker mother 10.0.2 1
