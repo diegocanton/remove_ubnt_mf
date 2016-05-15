862d6de35deddeb232854b51e23f3294769b5edc
diegocanton <diegocantonbgi@gmail.com>
2016-05-15 19:27:33 -0300

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -2,6 +2,9 @@
 # This script upgrade firmware
 # Colaboration: Alexandre Jeronimo Correa - Onda Internet
 
+#detect version of fw - Colaboration PVi1 (Git user)
+    versao=`mca-status  | grep firmware | cut -d, -f3 | cut -d= -f2 | cut -d. -f1`
+
 if [ "$versao" == "XM" ]; then
             URL='http://www.ubnt.com/downloads/XN-fw-internal/v5.6.5/XM.v5.6.5.29033.160515.2119.bin'
             wget $URL -O /tmp/XM.v5.6.5.29033.160515.2119.bin
