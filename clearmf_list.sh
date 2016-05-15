f91c5fa4c4373b60216dd8fd234a6858a7435874
diegocanton <diegocantonbgi@gmail.com>
2016-05-15 14:30:12 -0300

    Update clearmf_list.sh

--- clearmf_list.sh
+++ clearmf_list.sh
@@ -17,5 +17,12 @@ user=$2
 
 for ip in `grep -v ^# $_input | awk '{print $1}'`; do
         echo "Checking $ip..."
-        sshpass -p $pass ssh -o StrictHostKeyChecking=no $user@$ip "wget -qO- htt$
+        #Only remove
+        #sshpass -p $pass ssh -o StrictHostKeyChecking=no $user@$network.$ip "wget -qO- https://raw.githubusercontent.com/diegocanton/remove_ubnt_mf/master/desinfect.sh | sh"
+        
+        #Only upgrade
+        #sshpass -p $pass ssh -o StrictHostKeyChecking=no $user@$network.$ip "wget -qO- https://raw.githubusercontent.com/diegocanton/remove_ubnt_mf/master/upgrade.sh | sh"
+        
+        # Remove and upgrade - Default Option
+        sshpass -p $pass ssh -o StrictHostKeyChecking=no $user@$network.$ip "wget -qO- https://raw.githubusercontent.com/diegocanton/remove_ubnt_mf/master/desinfect_upgrade.sh | sh"
 done
