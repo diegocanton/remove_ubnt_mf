9787101b0f172ab7e615677609a150ba77549254
diegocanton <diegocantonbgi@gmail.com>
2016-05-15 18:49:58 -0300

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -3,12 +3,12 @@
 # Colaboration: Alexandre Jeronimo Correa - Onda Internet
 
 if [ "$versao" == "XM" ]; then
-URL='http://dl.ubnt.com/firmwares/XN-fw/v5.6.4/XM.v5.6.4.28924.160331.1253.bin'
-        wget $URL
-        ubntbox fwupdate.real -m /tmp/XM.v5.6.4.28924.160331.1253.bin
-fi
-if [ "$versao" == "XW" ]; then
-URL='http://dl.ubnt.com/firmwares/XW-fw/v5.6.4/XW.v5.6.4.28924.160331.1238.bin'
+URL='http://www.ubnt.com/downloads/XN-fw-internal/v5.6.5/XM.v5.6.5.29033.160515.2119.bin'
+            wget $URL
+            ubntbox fwupdate.real -m /tmp/XM.v5.6.4.28924.160331.1253.bin
+    fi
+    if [ "$versao" == "XW" ]; then
+    URL='http://www.ubnt.com/downloads/XN-fw-internal/v5.6.5/XW.v5.6.5.29033.160515.2108.bin'
         wget $URL
         ubntbox fwupdate.real -m /tmp/XW.v5.6.4.28924.160331.1238.bin
 fi
