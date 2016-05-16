61d385d34fd353ce72f467ceea8997e8f6852036
Joshua <c.treyce@gmail.com>
2016-05-16 10:52:54 -0600

    Changed download locations of firmware

--- fwupgrade.sh
+++ fwupgrade.sh
@@ -3,20 +3,12 @@
 mca_status=`mca-status | head -n 1 | awk -F, '{print $3}'`
 # firmwareVersion=XM.ar7240.v5.6.5.29033.160515.2119
 firmware=""
-fw_xm="XM.v5.6.5.29033.160515.2119.bin"
-fw_xw="XW.v5.6.5.29033.160515.2108.bin"
 
 cd /tmp
 
 # Clear bad downloads, etc..
 if [ -e "/tmp/fwupdate.bin" ] ; then
   rm /tmp/fwupdate.bin
-fi
-if [ -e "/tmp/$fw_xm" ] ; then
-  rm /tmp/$fw_xm
-fi
-if [ -e "/tmp/$fw_xw" ] ; then
-  rm /tmp/$fw_xw
 fi
 
 if ! echo $mca_status | grep -q "v5.6.5"; then
@@ -25,23 +17,21 @@ if ! echo $mca_status | grep -q "v5.6.5"; then
   # XM Radio
   if echo $mca_status | grep -q "firmwareVersion=XM"; then
     echo "	XM Radio Detected"
-    firmware=$fw_xm
+    firmware="http://www.ubnt.com/downloads/XN-fw-internal/v5.6.5/XM.v5.6.5.29033.160515.2119.bin"
   fi
 
   # XW Radio
   if echo $mca_status | grep -q "firmwareVersion=XW"; then
-    echo "	XM Radio Detected"
-    firmware=$fw_xw
+    echo "	XW Radio Detected"
+    firmware="http://www.ubnt.com/downloads/XN-fw-internal/v5.6.5/XW.v5.6.5.29033.160515.2108.bin"
   fi
 
   if [ ! -z "$firmware" ]; then
-    wget -q http://redstonelabs.net/$firmware
+    wget -q $firmware -O /tmp/fwupdate.bin
     if [ $? -ne 0 ]; then
       echo "  Download failed, stopping"
       rm /tmp/$firmware
     else
-      mv $firmware fwupdate.bin
-
       # Update firmware
       if [ -e "/tmp/fwupdate.bin" ] ; then
         echo "  Upgrading firmware"
