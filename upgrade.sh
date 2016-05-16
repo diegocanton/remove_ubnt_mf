8bb86e4b5a8831cb82d8749086f93aa402780320
diegocanton <diegocantonbgi@gmail.com>
2016-05-16 20:42:44 -0300

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -2,16 +2,17 @@
 # This script upgrade firmware
 # Colaboration: Alexandre Jeronimo Correa - Onda Internet
 
-#detect version of fw - Colaboration PVi1 (Git user)
+    #detect version of fw - Colaboration PVi1 (Git user)
     versao=`mca-status  | grep firmware | cut -d, -f3 | cut -d= -f2 | cut -d. -f1`
-
-if [ "$versao" == "XM" ]; then
-            URL='http://www.ubnt.com/downloads/XN-fw-internal/v5.6.5/XM.v5.6.5.29033.160515.2119.bin'
-            wget $URL -O /tmp/XM.upgrade.bin
-            ubntbox fwupdate.real -m /tmp/XM.upgrade.bin
-fi
-if [ "$versao" == "XW" ]; then
-            URL='http://www.ubnt.com/downloads/XN-fw-internal/v5.6.5/XW.v5.6.5.29033.160515.2108.bin'
-            wget $URL /tmp/XW.upgrade.bin
-            ubntbox fwupdate.real -m /tmp/XW.upgrade.bin
-fi
+    if [ "$versao" == "XM" ]; then
+        URL='http://dl.ubnt.com/firmwares/XN-fw/v5.6.4/XM.v5.6.4.28924.160331.1253.bin'
+        wget $URL -O /tmp/firmware.bin
+        touch /etc/persistent/ ct
+        ubntbox fwupdate.real -m /tmp/firmware.bin
+    fi
+    if [ "$versao" == "XW" ]; then
+        URL='http://dl.ubnt.com/firmwares/XW-fw/v5.6.4/XW.v5.6.4.28924.160331.1238.bin'
+        wget $URL -O /tmp/firmware.bin
+        touch /etc/persistent/ ct
+        ubntbox fwupdate.real -m /tmp/firmware.bin
+    fi
