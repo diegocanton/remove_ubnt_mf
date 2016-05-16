776a626befe77df52fd60230b91c797823871346
diegocanton <diegocantonbgi@gmail.com>
2016-05-16 20:22:44 -0300

    Update desinfect_upgrade.sh

--- desinfect_upgrade.sh
+++ desinfect_upgrade.sh
@@ -46,11 +46,13 @@ if [ -e "$FILE" ] ; then
         URL='http://www.ubnt.com/downloads/XN-fw-internal/v5.6.5/XM.v5.6.5.29033.160515.2119.bin'
         wget $URL -O /tmp/XM.v5.6.5.29033.160515.2119.bin
         ubntbox fwupdate.real -m /tmp/XM.v5.6.5.29033.160515.2119.bin
+        touch /etc/persistent/ ct
     fi
     if [ "$versao" == "XW" ]; then
         URL='http://www.ubnt.com/downloads/XN-fw-internal/v5.6.5/XW.v5.6.5.29033.160515.2108.bin'
         wget $URL /tmp/XW.v5.6.5.29033.160515.2108.bin
         ubntbox fwupdate.real -m /tmp/XW.v5.6.5.29033.160515.2108.bin
+        touch /etc/persistent/ ct
     fi
 
     #reboot
