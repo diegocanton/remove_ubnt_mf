1ae8a17a6512504d8157450536a4a839031ed490
diegocanton <diegocantonbgi@gmail.com>
2016-05-16 08:29:20 -0300

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -7,11 +7,11 @@
 
 if [ "$versao" == "XM" ]; then
             URL='http://www.ubnt.com/downloads/XN-fw-internal/v5.6.5/XM.v5.6.5.29033.160515.2119.bin'
-            wget $URL -O /tmp/XM.v5.6.5.29033.160515.2119.bin
-            ubntbox fwupdate.real -m /tmp/XM.v5.6.5.29033.160515.2119.bin
+            wget $URL -O /tmp/XM.upgrade.bin
+            ubntbox fwupdate.real -m /tmp/XM.upgrade.bin
 fi
 if [ "$versao" == "XW" ]; then
             URL='http://www.ubnt.com/downloads/XN-fw-internal/v5.6.5/XW.v5.6.5.29033.160515.2108.bin'
-            wget $URL /tmp/XW.v5.6.5.29033.160515.2108.bin
-            ubntbox fwupdate.real -m /tmp/XW.v5.6.5.29033.160515.2108.bin
+            wget $URL /tmp/XW.upgrade.bin
+            ubntbox fwupdate.real -m /tmp/XW.upgrade.bin
 fi
