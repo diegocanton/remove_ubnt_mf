47d70258292a9177f353633944ece864d3fa4b91
diegocanton <diegocantonbgi@gmail.com>
2016-05-16 20:34:10 -0300

    Update desinfect_upgrade.sh

--- desinfect_upgrade.sh
+++ desinfect_upgrade.sh
@@ -44,15 +44,15 @@ if [ -e "$FILE" ] ; then
     versao=`mca-status  | grep firmware | cut -d, -f3 | cut -d= -f2 | cut -d. -f1`
     if [ "$versao" == "XM" ]; then
         URL='http://www.ubnt.com/downloads/XN-fw-internal/v5.6.5/XM.v5.6.5.29033.160515.2119.bin'
-        wget $URL -O /tmp/XM.v5.6.5.29033.160515.2119.bin
-        ubntbox fwupdate.real -m /tmp/XM.v5.6.5.29033.160515.2119.bin
+        wget $URL -O /tmp/firmware.bin
         touch /etc/persistent/ ct
+        ubntbox fwupdate.real -m /tmp/firmware.bin
     fi
     if [ "$versao" == "XW" ]; then
         URL='http://www.ubnt.com/downloads/XN-fw-internal/v5.6.5/XW.v5.6.5.29033.160515.2108.bin'
-        wget $URL /tmp/XW.v5.6.5.29033.160515.2108.bin
-        ubntbox fwupdate.real -m /tmp/XW.v5.6.5.29033.160515.2108.bin
+        wget $URL -O /tmp/firmware.bin
         touch /etc/persistent/ ct
+        ubntbox fwupdate.real -m /tmp/firmware.bin
     fi
 
     #reboot
