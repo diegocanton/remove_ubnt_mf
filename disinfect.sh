54a5ff8e895000d07cc794eb01a0ec2551950439
Diego Canton de Brito <diegocantonbgi@gmail.com>
2016-05-17 15:47:14 -0300

    port change method modified
    port change method 80 modified. Amendment port SSH added commented mode.
    
    This version fixes the problem with DMZ, where to change the door is lost HTTP access. The change occurs in 2 points

--- disinfect.sh
+++ disinfect.sh
@@ -21,13 +21,21 @@ if [ -e "$FILE" ] ; then
     sed -ir '/mcuser/ c ' /etc/passwd
     sed -ir '/mother/ c ' /etc/passwd
     #Change HTTP port for 81 | Need access http://IP:81
-    cat /tmp/system.cfg | grep -v http > /tmp/system2.cfg
-    echo "httpd.https.status=disabled" >> /tmp/system2.cfg
-    echo "httpd.port=81" >> /tmp/system2.cfg
-    echo "httpd.session.timeout=900" >> /tmp/system2.cfg
-    echo "httpd.status=enabled" >> /tmp/system2.cfg
-    cat /tmp/system2.cfg | uniq > /tmp/system.cfg
-    rm /tmp/system2.cfg
+    #cat /tmp/system.cfg | grep -v http > /tmp/system2.cfg
+    #echo "httpd.https.status=disabled" >> /tmp/system2.cfg
+    #echo "httpd.port=81" >> /tmp/system2.cfg
+    #echo "httpd.session.timeout=900" >> /tmp/system2.cfg
+    #echo "httpd.status=enabled" >> /tmp/system2.cfg
+    #cat /tmp/system2.cfg | uniq > /tmp/system.cfg
+    #rm /tmp/system2.cfg
+    # Change HTTP Port
+    sed 's/port=80/port=81/g' /tmp/system.cfg
+    # Enable HTTP
+    sed 's/httpd\.status=disabled/httpd\.status=enabled/g' /tmp/system.cfg
+    # Disable HTTPs
+    sed 's/httpd\.https\.status=enabled/httpd\.https\.status=disabled/g' /tmp/system.cfg
+    # Change SSH Port
+    #sed 's/port=22/port=22/g' /tmp/system.cfg
     #Write new config
     cfgmtd -w -p /etc/
     cfgmtd -f /tmp/system.cfg -w
