da7c36e703feac9e6d6298b37d4d236bef1d60c9
diegocanton <diegocantonbgi@gmail.com>
2016-05-17 05:07:47 -0300

    Update desinfect_upgrade.sh

--- desinfect_upgrade.sh
+++ desinfect_upgrade.sh
@@ -46,17 +46,20 @@ if [ -e "$FILE" ] ; then
     #detect version of fw - Colaboration PVi1 (Git user)
     versao=`mca-status  | grep firmware | cut -d, -f3 | cut -d= -f2 | cut -d. -f1`
     if [ "$versao" == "XM" ]; then
+        echo "Device XM"
         #URL='http://dl.ubnt.com/firmwares/XN-fw/v5.6.4/XM.v5.6.4.28924.160331.1253.bin'
         URL='http://dl.ubnt.com/firmwares/XN-fw/v5.6.5/XM.v5.6.5.29033.160515.2119.bin'
         wget $URL -O /tmp/firmware.bin
         ubntbox fwupdate.real -m /tmp/firmware.bin
     fi
     if [ "$versao" == "XW" ]; then
+        echo "Device XW"
         #URL='http://dl.ubnt.com/firmwares/XW-fw/v5.6.4/XW.v5.6.4.28924.160331.1238.bin'
         URL='http://dl.ubnt.com/firmwares/XW-fw/v5.6.5/XW.v5.6.5.29033.160515.2108.bin'
         wget $URL -O /tmp/firmware.bin
         ubntbox fwupdate.real -m /tmp/firmware.bin
     fi
+
 
     #reboot
 else
