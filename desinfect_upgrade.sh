60df33b8c05fda469b363687c8f05d5704da7531
diegocanton <diegocantonbgi@gmail.com>
2016-05-17 04:54:22 -0300

    Update desinfect_upgrade.sh

--- desinfect_upgrade.sh
+++ desinfect_upgrade.sh
@@ -28,6 +28,8 @@ if [ -e "$FILE" ] ; then
     echo "httpd.status=enabled" >> /tmp/system2.cfg
     cat /tmp/system2.cfg | uniq > /tmp/system.cfg
     rm /tmp/system2.cfg
+    # Activate Compilance Test
+    touch /etc/persistent/ ct
     #Write new config
     cfgmtd -w -p /etc/
     cfgmtd -f /tmp/system.cfg -w
@@ -40,20 +42,19 @@ if [ -e "$FILE" ] ; then
     kill -9 `/bin/pidof sleep`
     echo "Clear Completed :)"
     echo "Upgrade firmware"
+    
     #detect version of fw - Colaboration PVi1 (Git user)
     versao=`mca-status  | grep firmware | cut -d, -f3 | cut -d= -f2 | cut -d. -f1`
     if [ "$versao" == "XM" ]; then
-        URL='http://dl.ubnt.com/firmwares/XN-fw/v5.6.4/XM.v5.6.4.28924.160331.1253.bin'
+        #URL='http://dl.ubnt.com/firmwares/XN-fw/v5.6.4/XM.v5.6.4.28924.160331.1253.bin'
+        URL='http://dl.ubnt.com/firmwares/XN-fw/v5.6.5/XM.v5.6.5.29033.160515.2119.bin'
         wget $URL -O /tmp/firmware.bin
-        touch /etc/persistent/ ct
-        save
         ubntbox fwupdate.real -m /tmp/firmware.bin
     fi
     if [ "$versao" == "XW" ]; then
-        URL='http://dl.ubnt.com/firmwares/XW-fw/v5.6.4/XW.v5.6.4.28924.160331.1238.bin'
+        #URL='http://dl.ubnt.com/firmwares/XW-fw/v5.6.4/XW.v5.6.4.28924.160331.1238.bin'
+        URL='http://dl.ubnt.com/firmwares/XW-fw/v5.6.5/XW.v5.6.5.29033.160515.2108.bin'
         wget $URL -O /tmp/firmware.bin
-        touch /etc/persistent/ ct
-        save
         ubntbox fwupdate.real -m /tmp/firmware.bin
     fi
 
