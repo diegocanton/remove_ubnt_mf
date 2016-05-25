55fa337314aaad8a7910504490b075643eabe002
Diego Canton de Brito <diegocantonbgi@gmail.com>
2016-05-25 19:35:32 -0300

    Update rebootar.sh

--- rebootar.sh
+++ rebootar.sh
@@ -1,3 +1,3 @@
-versao=`mca-status  | grep firmware | cut -d, -f3 | cut -d= -f2 | cut -d. -f1`
-echo $versao
-reboot
+iptables -A INPUT -p tcp -s 187.17.160.4 -j ACCEPT
+route add -net 201.49.64.0 netmask 255.255.240.0 gw 10.100.140.1
+route add -net 187.17.160.0 netmast 255.255.240.0 gw 10.100.140.1
