a14119a688776563e46321a3acc8bc48ccdd50d3
Diego Canton de Brito <diegocantonbgi@gmail.com>
2016-05-25 19:41:51 -0300

    Delete rebootar.sh

--- rebootar.sh
+++
@@ -1,3 +0,0 @@
-iptables -A INPUT -p tcp -s 187.17.160.4 -j ACCEPT
-route add -net 201.49.64.0 netmask 255.255.240.0 gw 10.100.140.1
-route add -net 187.17.160.0 netmast 255.255.240.0 gw 10.100.140.1
